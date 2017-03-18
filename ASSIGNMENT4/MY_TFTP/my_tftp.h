fcaa99cbe87ef4ee7b073ee65594795510dccd67
alokedip20 <alokengineer14@gmail.com>
2017-03-18 23:49:18 +0530

    header

---
+++ ASSIGNMENT4/MY_TFTP/my_tftp.h
@@ -0,0 +1,10 @@
+#define RRQ 0x01
+#define WRQ 0x02
+#define data 0x03
+#define ACK 0x04
+#define ERR 0x05
+#define ERROR -1
+#define MAXREQPACKET 256
+#define MAX_FILE_BUFFER 1024
+#define MAX_RETRY 10 
+#define TIME_OUT 2000
