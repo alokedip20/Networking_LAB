b3e9c899665e68dd47ad935d570db258ffb6c0ca
alokedip20 <alokengineer14@gmail.com>
2017-03-28 18:23:00 +0530

    updated

--- ASSIGNMENT4/MY_TFTP/my_tftp.h
+++ ASSIGNMENT4/MY_TFTP/my_tftp.h
@@ -7,7 +7,6 @@
 #define MAXREQPACKET 256
 #define MAX_FILE_BUFFER 1024
 #define MAX_RETRY 10 
-#define TIME_OUT 2000
 char ERROR_MESSAGE[8][256] = {	"Not defined, see error message (if any).\0",
 								"File not found.\0",
 								"Access violation.\0",
@@ -16,3 +15,4 @@ char ERROR_MESSAGE[8][256] = {	"Not defined, see error message (if any).\0",
 								"Unknown transfer ID.\0",
 								"File already exists.\0",
 								"No such user.\0"};
+struct timeval time_channel,start_time,end_time;
